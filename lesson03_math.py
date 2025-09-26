5f26d6330a9d0d8437904e1f5c9a03c8b971a762
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-09-26 18:02:11 +0000

    math operators complete, notes taken

---
+++ lesson03_math.py
@@ -0,0 +1,45 @@
+# KEY CONCEPTS: math operators: +, -, *, /, //, %, **
+
+add = 35 + 32
+print("Sum:", add)
+
+subtract = 100 - 33
+print("Difference:", subtract)
+
+multiply = 33.5 * 2
+print("Product:", multiply)
+
+float_divide = 7 / 2
+print("Float Division: ", float_divide)
+
+# floor division (integer division) always rounds down to the closest integer (//).
+integer_divide = 7 // 2
+print("Integer Division", integer_divide)
+
+# modulus: asking only for the remainder of a division (%)
+modulus = 7 % 2
+print("Modulus:", modulus)
+
+exponent = 7 ** 3
+print("Exponent:", exponent)
+
+# Example of boolean with math operators
+print(2 + 2 == 10 - 6) 
+
+# python follows the order of operations (PEMDAS)
+result1 = (2 + 3) * 4
+print("Result 1:", result1)
+
+result2 = 2 ** 3 * 4
+print("Result 2:", result2)
+
+result3 = 20 / 5 * 2
+print("Result 3:", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4:", result4)
+
+result5 = 5 + 2 ** 3 * (4 - 1)
+print("Result 5:", result5)
+
+# general math note: multiplication, division, addition, and subtraction work left to right in PEMDAS
