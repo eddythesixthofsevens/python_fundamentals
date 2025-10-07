33e38222cab4892c481eb265a1eed7b169313457
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-10-07 16:22:31 +0000

    built in func 1

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -1,17 +1,37 @@
-# Basic string creation and indexes
-greeting = "hello"
-name = "ada"
+# # Basic string creation and indexes
+# greeting = "hello"
+# name = "ada"
 
-# 0 1 2 3 4
-# H e l l o
+# # 0 1 2 3 4
+# # H e l l o
 
-message = greeting + " " + name + "!!!"
+# message = greeting + " " + name + "!!!"
 
-print("Concatenated message: ", message)
-print()
-print(greeting[1])
-print()
-word = "super - cali - fragil - istic - expi - ali - docious"
-print("First letter of word: ", word[0])
-print("Last letter of word: ", word[-1])
-print("range of letters (non inclusive): ", word[0:6])
+# print("Concatenated message: ", message)
+# print()
+# print(greeting[1])
+# print()
+# word = "super - cali - fragil - istic - expi - ali - docious"
+# print("First letter of word: ", word[0])
+# print()
+# print("Last letter of word: ", word[-1])
+# print()
+# print("range of letters (non inclusive): ", word[0:6])
+# print()
+# print("another range of letters: ", word[6:12])
+# print()
+# print(word[:5])
+# print()
+# print("Last seven letters: ", word[-7:])
+# print()
+# print("step through every second letter of the big word: ", word[::-2])
+# print()
+# print("reverse word: ", word[::-1])
+# print()
+# print("weird word: ", word[::-3])
+
+# built in functions
+
+country = "egypt"
+length_of_word = len(country)
+print(length_of_word)
