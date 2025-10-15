1100c7cfc931f53805c09aa89781b1c78b57930c
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-10-15 16:59:09 +0000

    challenges complete!

--- lesson07_conditionals.py
+++ lesson07_conditionals.py
@@ -58,16 +58,63 @@
 # else:
 #     print("Driving is not permitted as of now.")
 # using 'or' and 'not'
-import random
-day = random.choice(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"])
-if day == "saturday" or day == "sunday":
-    print("It's the weekend!")
-elif day is "monday":
-    print("Suffering begins.")
-else:
-    print("Your suffering shall continue.")
+# import random
+# day = random.choice(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"])
+# if day == "saturday" or day == "sunday":
+#     print("It's the weekend!")
+# elif day is "monday":
+#     print("Suffering begins.")
+# else:
+#     print("Your suffering shall continue.")
 
-if day is not "monday":
-    print("THE SUFFERING IS REDUCED!")
+# if day is not "monday":
+#     print("THE SUFFERING IS REDUCED!")
 
 # can use is in place of ==
+
+# Challenge 1: Even or Odd
+# Ask the user for a number, and tell them if it’s even or odd.
+# Example output:
+# Enter a number: 7
+# 7 is odd.
+# Hint: use modulus operator
+
+# number = int(input("Enter a number: "))
+# even_or_odd = number % 2
+# if even_or_odd == 1:
+#     print("Your number is odd.")
+# else:
+#     print("Your number is even.")
+
+# Challenge 2: Password Check
+# Create a variable with a stored password
+# Ask the user to enter a password. 
+# If it matches the stored password, print "Access granted." Otherwise, "Access denied."
+# Example output:
+# Enter password: dolphin
+# Access granted. Access denied.
+# Enter password: swordfish
+# Access granted.
+
+# password = "pyramids!"
+# insert_pass = input("Enter a password: ")
+# if password == insert_pass:
+#     print("Access granted.")
+# else:
+#     print("Access denied.")
+
+# Challenge 3: Grading System
+# Ask the user for a numeric grade (0-100) and print a letter grade.
+# Example output:
+# Enter your grade: 94
+# You earned an A.
+
+grade = int(input("Enter a grade (1-100): "))
+if grade >= 90:
+    print("A")
+elif grade >= 80 and grade < 90:
+    print("B")
+elif grade >= 70 and grade < 80:
+    print("C")
+else:
+    print("F")
