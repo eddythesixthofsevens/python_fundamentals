85f99e65e6b1f0f6ecf32ae52fa89442fb238652
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-10-21 17:06:41 +0000

    challenges complete

---
+++ challenges_01.py
@@ -0,0 +1,29 @@
+# Challenge 1
+# word = input("Enter a word: ")
+# palindrome = word[::-1]
+
+# if word == palindrome:
+#     print(f"{word.capitalize()} is a palindrome!")
+# else:
+#     print(f"{word.capitalize()} is not a palindrome.")
+
+# Challenge 2
+# email = input("Enter your email:")
+# domainsymbol = email.index("@") + 1
+# domain = email[domainsymbol::]
+# print(domain)
+
+# Challenge 3
+# given_list = ["chicken", "jockey", "fork"]
+# last = given_list[2]
+# guess = input("Enter a thing: ")
+# print(last == guess)
+
+# Challenge 4
+# word2 = input("Enter a word: ")
+# if len(word2) >= 3 and "ing" not in word2:
+#     print(word2 + "ing")
+# elif len(word2) >= 3 and "ing" in word2:
+#     print(word2 + "ly")
+# else:
+#     print(word2)
